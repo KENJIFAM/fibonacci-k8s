faf5bc6f2606291e31905e2c7ca051d5f7a15516
Khoa Pham <kenjifam@gmail.com>
2019-02-19 22:20:47 +0200

    travis deployment setup

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t kenjifam/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kenjifam/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kenjifam/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+  
+docker push kenjifam/multi-client:latest
+docker push kenjifam/multi-server:latest
+docker push kenjifam/multi-worker:latest
+
+docker push kenjifam/multi-client:$SHA
+docker push kenjifam/multi-server:$SHA
+docker push kenjifam/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=kenjifam/multi-server:$SHA
+kubectl set image deployments/client-deployment client=kenjifam/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=kenjifam/multi-worker:$SHA
