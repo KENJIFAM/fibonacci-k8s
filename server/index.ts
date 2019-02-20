2f905c0132e5fa7706f2e920bd479dd32267fcec
Khoa Pham <kenjifam@gmail.com>
2019-02-20 03:44:19 +0200

    fix postgres reject

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+  .catch(err => process.exit(1));
 
 // Redis Client
 const redisClient: RedisClient = redis.createClient({
